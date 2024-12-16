0c9fc039dbbfe2c7c0b317d9bf23a2d5a90fcfe9
tominagalab <tominaga@kct.ac.jp>
2024-12-16 12:36:45 +0000

    delete py

--- src/sample_imread.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import cv2
-img = cv2.imread('/home/ubuntu/catkin_ws/src/chinorobo_ros_opencv_01/resource/img01.jpg')
-cv2.imshow('input', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
